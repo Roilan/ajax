68bdfae68598eb97a4d35ec23ff1f9a778639549
Roilan <roilan14@gmail.com>
2014-10-28 17:14:55 -0400

    some styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,27 +5,25 @@ $('document').ready(function() {
 
 	$submit.on('click', function(e) {
 		e.preventDefault();
-		
-		var url = 'http://www.reddit.com/r/aww.json?jsonp=';
-
-		function callback(data) {
-			// obj = data.data.children
-			console.log(data.data.children);
-
-			var html = '<ul>';
-
-			$.each(data.data.children, function(i, redditPost) {
-				html += '<li>';
-				html += '<img src="' + redditPost.data.thumbnail +  '" alt="alt txt" />';
-				html += '</li>';
-			});
-			html += '</ul>';
-			$username.html(html);
-			$('#username li').first().remove();
-		};
-
-		$.getJSON(url, callback);
 	});
 
+
+	var url = 'http://www.reddit.com/r/aww.json?jsonp=';
+
+	function callback(data) {
+		// obj = data.data.children
+
+		var html = '<ul id="ul-style">';
+
+		$.each(data.data.children, function(i, redditPost) {
+			html += '<li class="li-style">';
+			html += '<img src="' + redditPost.data.thumbnail +  '" alt="alt txt" />';
+			html += '</li>';
+		});
+		html += '</ul>';
+		$username.html(html);
+	};
+
+	$.getJSON(url, callback);
 
 }); // end ready

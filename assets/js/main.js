c6aa6c4ccb507b88aa943087e495e096570c8a31
Roilan <roilan14@gmail.com>
2014-10-28 21:30:51 -0400

    GET most upvoted subreddit

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,25 +5,35 @@ $('document').ready(function() {
 
 	$submit.on('click', function(e) {
 		e.preventDefault();
+		// find the most upvoted for a sub reddit
+
+		var url = 'http://www.reddit.com/r/' + $input.val() +'/top.json?jsonp=';
+
+		var settings = {
+			t: 'all',
+			limit: 10
+		};
+
+		function callback(data) {
+			// obj = data.data.children
+
+			var html = '<div>';
+
+			$.each(data.data.children, function(i, redditPost) {
+				html += '<p>';
+				html += '<a href="' + redditPost.data.url + ' target="_blank">' + redditPost.data.title  +  '</a>"';
+				html += ' <span style="color:red">'; 
+				html +=  redditPost.data.score;
+				html += '</span>';
+				html += '</p>';
+			});
+
+			html+= '</div>';
+			$username.html(html);
+		};
+
+		$.getJSON(url, settings,  callback);
+
 	});
-
-
-	var url = 'http://www.reddit.com/r/aww.json?jsonp=';
-
-	function callback(data) {
-		// obj = data.data.children
-
-		var html = '<ul id="ul-style">';
-
-		$.each(data.data.children, function(i, redditPost) {
-			html += '<li class="li-style">';
-			html += '<img src="' + redditPost.data.thumbnail +  '" alt="alt txt" />';
-			html += '</li>';
-		});
-		html += '</ul>';
-		$username.html(html);
-	};
-
-	$.getJSON(url, callback);
 
 }); // end ready

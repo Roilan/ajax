02b211c4e28f0227e4f21fb91726520ba9f6043f
Roilan <roilan14@gmail.com>
2014-11-10 15:33:44 -0500

    push

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,30 @@
 $('document').ready(function() {
 	var url = 'http://www.reddit.com/.json?jsonp=';
 
-		function addThumbnail() {
-			var r = redditFeed.data.thumbnail;
-
-			if (r == "") {
-				postarea += '<img class="thumbnail" src="assets/img/empty.png" />'
-				} else if (r == "self") {
-					postarea += '<img class="thumbnail" src="assets/img/self.png" />'
-				} else if (r == "nsfw") {
-					postarea += '<img class="thumbnail" src="assets/img/nsfw.png" />'
-				} else {
-					postarea += '<img class="thumbnail" src="' + r + '" />'
-			}
+	function addThumbnail() {
+		var r = redditFeed.data.thumbnail;
+	
+		if (r == "") {
+			postarea += '<img class="thumbnail" src="assets/img/empty.png" />'
+			} else if (r == "self") {
+				postarea += '<img class="thumbnail" src="assets/img/self.png" />'
+			} else if (r == "nsfw") {
+				postarea += '<img class="thumbnail" src="assets/img/nsfw.png" />'
+			} else {
+				postarea += '<img class="thumbnail" src="' + r + '" />'
 		}
+	}
 
 	function callback(data) {
+		var d;
+		//d = new Date(data.children.data.created * 1000);
 
 		var source = $('#reddit-feed').html();
 		var template = Handlebars.compile(source);
 		var mytemp = template(data);
 		
 		$('.postarea').html(mytemp);
+		//console.log(data.data.children[0].data.created);
 	}
 
 	$.getJSON(url, callback);
